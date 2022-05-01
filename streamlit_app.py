2b4278c2bbb1f610a98d7a309bd8b322cdc31943
nberney <nicolas@berney.biz>
2022-05-01 22:29:08 +0200

    Update streamlit_app.py
    Change the functions title to header and text for some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocket Smoothie')
-streamlit.title('Hard-Boiled Free-Range Egg')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
